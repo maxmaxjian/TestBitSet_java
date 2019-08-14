66d7921a3b2eb9f411342ff7c6f9f1d5820cfd4b
Jian Wei <wejian@amazon.com>
2019-08-14 17:57:33 -0400

    first submission

---
+++ src/main/java/Solution.java
@@ -0,0 +1,9 @@
+import java.util.BitSet;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        BitSet bset = new BitSet(1);
+        System.out.println("size of bset = " + bset.size());
+    }
+}
